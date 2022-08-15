32944900eee4931d614eb2028442de1a00ab18c9
Raspreet Singh <raspreet800@gmail.com>
2022-08-16 03:56:04 +0530

    replace i3status with i3status-rust

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -16,7 +16,6 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     number = true, -- Show numberline
     preserveindent = true, -- Preserve indent structure as much as possible
     pumheight = 10, -- Height of the pop up menu
-    relativenumber = true, -- Show relative numberline
     scrolloff = 8, -- Number of lines to keep above and below the cursor
     shiftwidth = 2, -- Number of space inserted for indentation
     showmode = false, -- Disable showing modes in command line
